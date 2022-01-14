711ee8a9aa2dfc6ffdc7847b55fb142750162c69
richard483 <richard.william483@gmail.com>
2022-01-14 10:07:09 +0700

    fitur pertambahan

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ public class Main {
 	public Main() {
 		System.out.println("Hello World");
 		System.out.println("Selamat datang di pertemuan 7 Mobile!"); //kita meng-echo ucapan selamat datang
+		
+		int sepuluh = 10, sebelas = 11;
+		System.out.println(sepuluh + sebelas);
 	}
 
 	public static void main(String[] args) {
