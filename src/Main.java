87482987c12027330fedc5367b94840bd7fc51db
richard483 <richard.william483@gmail.com>
2022-01-14 10:03:05 +0700

    menambahkan code selamat datang

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 
 	public Main() {
 		System.out.println("Hello World");
+		System.out.println("Selamat datang di pertemuan 7 Mobile!"); //kita meng-echo ucapan selamat datang
 	}
 
 	public static void main(String[] args) {
